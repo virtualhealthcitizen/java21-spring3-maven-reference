8731202043c3deafc7ae26488c7fa7d4d9538e42
James Morse <morse.james.r@gmail.com>
2025-10-16 21:16:47 -0500

    Edit project dependencies

--- google-cloud-sql/src/main/java/org/squidmin/java/spring/maven/cloudsql/GoogleCloudSqlApplication.java
+++ google-cloud-sql/src/main/java/org/squidmin/java/spring/maven/cloudsql/GoogleCloudSqlApplication.java
@@ -1,4 +1,4 @@
-package org.squidmin.java.spring.maven.cloudsql.googlecloudsql;
+package org.squidmin.java.spring.maven.cloudsql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- google-cloud-sql/src/test/java/org/squidmin/java/spring/maven/cloudsql/GoogleCloudSqlApplicationTests.java
+++ google-cloud-sql/src/test/java/org/squidmin/java/spring/maven/cloudsql/GoogleCloudSqlApplicationTests.java
@@ -1,4 +1,4 @@
-package org.squidmin.java.spring.maven.cloudsql.googlecloudsql;
+package org.squidmin.java.spring.maven.cloudsql;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
