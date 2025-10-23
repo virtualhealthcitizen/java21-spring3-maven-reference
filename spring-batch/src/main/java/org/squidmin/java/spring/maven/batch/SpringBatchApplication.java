7543e6e0c8051eb5ce8e69c31734c159e7306c1a
James Morse <morse.james.r@gmail.com>
2025-10-22 21:39:06 -0500

    Edit project modules

--- spring-batch/src/main/java/org/squidmin/java/spring/maven/batch/SpringBatchApplication.java
+++ spring-batch/src/main/java/org/squidmin/java/spring/maven/batch/SpringBatchApplication.java
@@ -2,10 +2,8 @@ package org.squidmin.java.spring.maven.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = "org.squidmin.java.spring.maven.batch")
-@EntityScan(basePackages = "org.squidmin.java.spring.maven.batch.domain")
+@SpringBootApplication
 public class SpringBatchApplication {
 
     public static void main(String[] args) {
