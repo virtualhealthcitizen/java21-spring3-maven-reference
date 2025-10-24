91067fd821de7386ba83a320799b786eae00d2b2
James Morse <morse.james.r@gmail.com>
2025-10-23 20:23:54 -0500

    Edit pubsub module

--- google-cloud-pubsub/src/main/java/org/squidmin/java/spring/maven/pubsub/config/PubSubConfig.java
+++ google-cloud-pubsub/src/main/java/org/squidmin/java/spring/maven/pubsub/config/PubSubConfig.java
@@ -1,11 +1,9 @@
 package org.squidmin.java.spring.maven.pubsub.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Slf4j
 public class PubSubConfig {
 
     private final String projectId;
--- google-cloud-pubsub/src/test/java/org/squidmin/java/spring/maven/pubsub/config/PubSubConfigTest.java
+++ google-cloud-pubsub/src/test/java/org/squidmin/java/spring/maven/pubsub/config/PubSubConfigTest.java
@@ -1,6 +1,5 @@
 package org.squidmin.java.spring.maven.pubsub.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +8,6 @@ import org.squidmin.java.spring.maven.pubsub.UnitTestConfig;
 
 @SpringBootTest(classes = {PubSubConfig.class, UnitTestConfig.class})
 @ActiveProfiles("test")
-@Slf4j
 public class PubSubConfigTest {
 
     @Autowired
@@ -20,6 +18,9 @@ public class PubSubConfigTest {
         System.out.println("Project ID: " + pubSubConfig.getProjectId());
         System.out.println("Topic: " + pubSubConfig.getTopic());
         System.out.println("Subscription: " + pubSubConfig.getSubscription());
+        System.out.println("Role: " + pubSubConfig.getRole());
+        System.out.println("Ordering Key: " + pubSubConfig.getOrderingKey());
+        System.out.println("Max Retries: " + pubSubConfig.getMaxRetries());
     }
 
 }
