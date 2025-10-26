fd5d33a305163be2d04e7b869aa08a3e2ce73b06
James Morse <morse.james.r@gmail.com>
2025-10-26 12:55:57 -0500

    CI workflow (cont'd)

--- google-cloud-storage/src/test/java/org/squidmin/java/spring/maven/gcs/GoogleCloudStorageApplicationTests.java
+++ google-cloud-storage/src/test/java/org/squidmin/java/spring/maven/gcs/GoogleCloudStorageApplicationTests.java
@@ -2,8 +2,10 @@ package org.squidmin.java.spring.maven.gcs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = {UnitTestConfig.class})
+@ActiveProfiles("test")
 class GoogleCloudStorageApplicationTests {
 
     @Test
---
+++ google-cloud-storage/src/test/java/org/squidmin/java/spring/maven/gcs/UnitTestConfig.java
@@ -0,0 +1,18 @@
+package org.squidmin.java.spring.maven.gcs;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+
+@Configuration
+@ActiveProfiles("test")
+public class UnitTestConfig {
+
+    @Bean
+    public GoogleCredentials googleCredentials() {
+        return Mockito.mock(GoogleCredentials.class);
+    }
+
+}
