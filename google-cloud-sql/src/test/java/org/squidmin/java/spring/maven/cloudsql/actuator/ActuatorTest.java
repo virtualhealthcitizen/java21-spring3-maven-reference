ee8a4fee7708031affeedad85f88944cb5d957bb
James Morse <morse.james.r@gmail.com>
2025-10-26 21:10:19 -0500

    Update tests

--- google-cloud-sql/src/test/java/org/squidmin/java/spring/maven/cloudsql/actuator/ActuatorTest.java
+++ google-cloud-sql/src/test/java/org/squidmin/java/spring/maven/cloudsql/actuator/ActuatorTest.java
@@ -13,8 +13,10 @@ import static org.assertj.core.api.Assertions.assertThat;
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {
         "management.endpoints.web.exposure.include=health,info",
-        // show details so JSON has components (optional)
-        "management.endpoint.health.show-details=always"
+        "management.endpoint.health.show-details=always",
+        "spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.jpa.hibernate.ddl-auto=none"
     }
 )
 public class ActuatorTest {
