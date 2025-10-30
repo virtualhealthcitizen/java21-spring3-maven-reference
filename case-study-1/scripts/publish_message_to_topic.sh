fed704a8f012ca2d346ca64ff480767faf1555ed
James Morse <morse.james.r@gmail.com>
2025-10-29 21:59:10 -0500

    Delete unused code

--- case-study-1/scripts/publish_message_to_topic.sh
+++ case-study-1/scripts/publish_message_to_topic.sh
@@ -3,7 +3,6 @@ PROJECT_ID="lofty-root-378503"
 TOPIC_ID="java21-spring3-maven-reference-topic"
 
 # Payload must be base64. On macOS:
-#DATA_BASE64="$(printf '{"id":"9a7b127e-23b5-48a1-93dc-03c5c619c6b3","name":"Full Widget","createdAt":"2023-10-01T12:00:00Z","meta":{"color":"blue","size":"large","features":["waterproof","shockproof"]}}' | base64 | tr -d '\n')"
 DATA_BASE64="$(printf '{"eventId":"9a7b127e-23b5-48a1-93dc-03c5c619c6b3","timestamp":"2023-10-01T12:00:00Z","messageType":"WIDGET_CREATED","records":[{"id":"9a7b127e-23b5-48a1-93dc-03c5c619c6b3","name":"Full Widget","createdAt":"2023-10-01T12:00:00Z","meta":{"color":"blue","size":"large","features":["waterproof","shockproof"]}}]}' | base64 | tr -d '\n')"
 
 # Get an OAuth2 access token via gcloud
--- case-study-1/src/main/java/org/squidmin/java/spring/maven/casestudies/casestudy1/subscribe/ExampleSubscriber.java
+++ case-study-1/src/main/java/org/squidmin/java/spring/maven/casestudies/casestudy1/subscribe/ExampleSubscriber.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
-import org.squidmin.java.spring.maven.casestudies.casestudy1.domain.Widget;
 import org.squidmin.java.spring.maven.casestudies.casestudy1.dto.WidgetPayload;
 import org.squidmin.java.spring.maven.casestudies.casestudy1.util.Loggable;
 
@@ -28,7 +27,6 @@ public class ExampleSubscriber {
     public Consumer<Message<String>> ingest() {
         return message -> {
             try {
-//                Widget widget = objectMapper.readValue(message.getPayload(), Widget.class);
                 WidgetPayload payload = objectMapper.readValue(message.getPayload(), WidgetPayload.class);
                 log.info("Received message payload: {}", objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(payload));
             } catch (JsonProcessingException e) {
